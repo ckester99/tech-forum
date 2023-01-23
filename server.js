c31b67b0a309bf11af87d4b9c5097a472c5ddbf8
Christian Kester <christiankester77@gmail.com>
2023-01-23 18:32:14 -0500

    added and tested handlebars engine

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const routes = require("./controllers");
 const path = require("path");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
