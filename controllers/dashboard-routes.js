077efec9cabf5954cae752fcfe7dabc274c814e9
Christian Kester <christiankester77@gmail.com>
2023-01-28 17:36:14 -0500

    added dashboard

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { User, ForumPost, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+    try {
+        const forumPosts = await ForumPost.findAll({ where: { user_id: req.session.user_id }, include: User });
+        const forumPostsPlain = forumPosts.map((post) => post.get({ plain: true }));
+        const comments = await Comment.findAll({ where: { user_id: req.session.user_id }, include: User });
+        const commentsPlain = comments.map((comment) => comment.get({ plain: true }));
+        const posts_and_comments = forumPostsPlain.concat(commentsPlain).sort((a, b) => {
+            return b.timestamp - a.timestamp;
+        });
+
+        res.render("dashboard", { user_id: req.session.user_id, posts_and_comments: posts_and_comments });
+    } catch (err) {
+        res.send(400).send(err);
+    }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const homeRoutes = require("./home-routes.js");
 const api = require("./api");
 const forumPostRoutes = require("./forum-post-routes");
+const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/", homeRoutes);
 router.use("/api", api);
 router.use("/forum-post", forumPostRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
